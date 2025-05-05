bf973429281e709145e37072c5b80db61015b0b5
misdarmanto <misdarmanto1922@gmail.com>
2025-05-05 10:43:04 +0700

    init

---
+++ apps/controllers/upload-file/index.js
@@ -0,0 +1,20 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.uploadFile = void 0;
+const response_1 = require("../../utilities/response");
+const http_status_codes_1 = require("http-status-codes");
+const configs_1 = require("../../configs");
+const uploadFile = async (req, res) => {
+    try {
+        const filePath = req.file.path;
+        const fileUrl = `${configs_1.CONFIG.appUrl}/${filePath}`;
+        console.log(filePath);
+        res.json({ message: 'File uploaded successfully!', filePath });
+    }
+    catch (error) {
+        const message = `unable to process request! error ${error.message}`;
+        const response = response_1.ResponseData.error(message);
+        return res.status(http_status_codes_1.StatusCodes.INTERNAL_SERVER_ERROR).json(response);
+    }
+};
+exports.uploadFile = uploadFile;
